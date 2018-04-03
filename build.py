233b9373b8fe53f707e30d47275b144411a8d073
Carlos Fernandez <cfern1990@gmail.com>
2018-04-02 20:31:48 -0400

    Added warning for Python versions older than 3.6
    Python 3.6 dictionaries preserve insertion order, and python 3.7 added it to the spec officially. Older versions of python won't maintain order when importing data for the build.

--- build.py
+++ build.py
@@ -338,6 +338,15 @@ def build_monster_rewards(session : sqlalchemy.orm.Session):
 
     print("Built Monster Rewards")
 
+import sys
+
+# Python 3.6 dictionaries preserve insertion order, and python 3.7 added it to the spec officially
+# Older versions of python won't maintain order when importing data for the build.
+if sys.version_info < (3,6):
+    print(f"WARNING: You are running python version {sys.version}, " +
+        "but this application was designed for Python 3.6 and newer. ")
+    print("Earlier versions of Python will still build the project, but will not have a consistent build.")
+    print("When creating a final build, make sure to use a newer version of python.")
 
 sessionbuilder = db.recreate_database(output_filename)
 
