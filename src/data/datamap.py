9689b088db829a22775765f2b3abb72d4227102a
Carlos Fernandez <cfern1990@gmail.com>
2018-04-02 20:20:31 -0400

    Added new internal use set_value method to DataMap
    "Internal use" methods are the methods I use personally when adding or merging data behind the scenes. This particular method will allow me to add fields in the middle without too much trouble. For example, it will allow me to add "description" after "name" in the base map.

--- src/data/datamap.py
+++ src/data/datamap.py
@@ -25,6 +25,29 @@ class DataRow(MutableMapping):
         "Returns a collection of (language, name) tuples for this row"
         for (lang, name) in self['name'].items():
             yield (lang, name)
+
+    def set_value(self, key, value, *, after=""):
+        """"Sets a value in this dictionary. 
+        Same as using [key]=value, but allows an item to be placed after another"""
+        if not after:
+            self[key] = value
+            return
+
+        keys_to_move = []
+        found_item = False
+        for item_key in self._data.keys():
+            if found_item:
+                keys_to_move.append(item_key)
+            elif item_key == after:
+                found_item = True
+
+        self[key] = value
+
+        # Move every entry to the end of the list
+        for item_key in keys_to_move:
+            value = self._data[item_key]
+            del self._data[item_key]
+            self._data[item_key] = value
         
     def __getitem__(self, key):
         return self._data[key]
