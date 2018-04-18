b408e9cf64f80db89b4e0f6f9307494b902bb748
Carlos Fernandez <cfern1990@gmail.com>
2018-04-17 20:31:54 -0400

    Added integration test to ensure that it builds successfuly

---
+++ tests/test_build.py
@@ -0,0 +1,14 @@
+import os
+import os.path
+
+import build
+
+def test_builds(tmpdir):
+    "Integration test to ensure the database builds"
+
+    fname = tmpdir.join('tmpdb.sql')
+    print(fname)
+    build.build_database(fname)
+
+    dbexists = os.path.exists(fname)
+    assert dbexists, 'Database should have been created'
