a18e8404692f965938ef9539e7474742e48338fd
Carlos Fernandez <cfern1990@gmail.com>
2018-05-29 02:45:43 -0400

    Data loading uses proper sibling import

--- mhdata/load/loaddata.py
+++ mhdata/load/loaddata.py
@@ -2,18 +2,14 @@ import os.path
 from os.path import abspath, join, dirname
 from types import SimpleNamespace
 
-
 from mhdata.io import DataMap, DataReader, DataStitcher
 
-# NOTE: Python doesn't seem to have a way to import siblings under a name
-# Absolute import fails if you are importing via nested package
-# If there is a way to access as cfg.* and schema.* I'm all ears.
-from .cfg import *
-from .schema import *
+from . import cfg
+from . import schema
 
 reader = DataReader(
-    required_languages=required_languages,
-    languages=list(supported_languages), 
+    required_languages=cfg.required_languages,
+    languages=list(cfg.supported_languages), 
     data_path=join(dirname(abspath(__file__)), '../../source_data')
 )
 
@@ -35,13 +31,13 @@ def load_data():
     result = SimpleNamespace()
 
     item_map = reader.load_base_csv("items/item_base.csv", groups=['description'])
-    result.item_map = transform_dmap(item_map, ItemSchema())
+    result.item_map = transform_dmap(item_map, schema.ItemSchema())
 
     location_map = (DataStitcher(reader, dir="locations/")
                     .base_csv('location_base.csv')
                     .add_csv("location_items.csv", key="items")
                     .get())
-    result.location_map = transform_dmap(location_map, LocationSchema())
+    result.location_map = transform_dmap(location_map, schema.LocationSchema())
 
     result.skill_map = reader.load_base_json("skills/skill_base.json")
     result.charm_map = reader.load_base_json('charms/charm_base.json')
@@ -61,18 +57,18 @@ def load_data():
                     .base_csv("armor_base.csv")
                     .add_json("armor_ext.json")
                     .get())
-    result.armor_map = transform_dmap(armor_map, ArmorSchema())
+    result.armor_map = transform_dmap(armor_map, schema.ArmorSchema())
 
     result.armorset_map = transform_dmap(
         reader.load_base_csv("armors/armorset_base.csv"),
-        ArmorSetSchema()
+        schema.ArmorSetSchema()
     )
 
     armorset_bonus_map = (DataStitcher(reader, dir="armors/")
                     .base_csv("armorset_bonus_base.csv")
                     .add_csv("armorset_bonus_skills.csv", key="skills")
                     .get())
-    result.armorset_bonus_map = transform_dmap(armorset_bonus_map, ArmorSetBonus())
+    result.armorset_bonus_map = transform_dmap(armorset_bonus_map, schema.ArmorSetBonus())
 
     # todo: stitch
     result.weapon_map = reader.load_base_json("weapons/weapon_base.json")
